04f504eae83445c8599a868adb1a0f6507b08f3e
zenojr <zfrancajunior@gmail.com>
2016-04-13 22:00:47 -0300

    IMC
    exercicios

--- INTRODUCAO/projeto/calcula-imc.js
+++ INTRODUCAO/projeto/calcula-imc.js
@@ -1,14 +1,21 @@
-var peso = 50;
-var altura = 2.0;
 
-console.log(peso);
-console.log(altura);
+var peso = document.getElementById("peso-1").textContent; //textContent seta o valor coletado no DOM para a variavel
+var altura = document.getElementById("altura-1").textContent;
 
 
-if (altura > 0) {
+
+
+if (altura != 0) {
     var imc = peso / (altura * altura);
+
+    var tdImc = document.getElementById("imc-2");
+    
+    tdImc.textContent = imc; 
+
     console.log(imc);
 
 } else {
   console.log("informe altura");
 }
+
+
