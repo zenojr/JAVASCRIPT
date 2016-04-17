b3c632d48b702652d443aa1b36d46345706c20ba
zenojr <zfrancajunior@gmail.com>
2016-04-17 15:02:12 -0300

    retomando estudos
    ARRAYS

---
+++ INTRODUCAO/ovosLeite.js
@@ -0,0 +1,8 @@
+var leite = 0;
+var ovos = true;
+
+while (ovos == true && leite <= 6) {
+	leite ++;
+}
+
+window.alert("Vá para casa!");
--- INTRODUCAO/projeto/calcula-imc.js
+++ INTRODUCAO/projeto/calcula-imc.js
@@ -1,15 +1,30 @@
 
-var peso = document.getElementById("peso-1").textContent; //textContent seta o valor coletado no DOM para a variavel
-var altura = document.getElementById("altura-1").textContent;
+//textContent seta o valor coletado no DOM para a variavel
+
+var tdPeso1 = document.getElementById("peso-1");
+var tdAltura1 = document.getElementById("altura-1");
+var paciente1 = { peso : tdPeso1.textContent, altura : tdAltura1.textContent};
+//primeiro paciente FIM
+
+var tdPeso2 = document.getElementById("peso-2");
+var tdAltura2 = document.getElementById("altura-2");
+var paciente2 = { peso : tdPeso2.textContent, altura : tdAltura2.textContent};
+//segundo paciente FIM 
+
+var pacientes = [paciente1, paciente2];
+var posicaoAtual = 0;
+
+var primeiro = pacientes[0];
+var segundo = pacientes[1];
+
+while (posicaoAtual <= pacientes.length -1) {
+	// statement
 
 
-
-
-if (altura != 0) {
-    var imc = peso / (altura * altura);
+if (paciente.altura != 0) {
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
 
     var tdImc = document.getElementById("imc-2");
-    
     tdImc.textContent = imc; 
 
     console.log(imc);
@@ -18,4 +33,4 @@ if (altura != 0) {
   console.log("informe altura");
 }
 
-
+}//endWhile
