3d4b75267cc13709d48d8693b3ff109a25d82397
zenojr <zfrancajunior@gmail.com>
2016-04-17 19:01:15 -0300

    function anonima
    função anônima adicionada no código dentro de um DOM de forma que a
    function não esteja acessível de forma Global

--- INTRODUCAO/projeto/calcula-imc.js
+++ INTRODUCAO/projeto/calcula-imc.js
@@ -1,16 +1,3 @@
-function calculaImc(paciente){
-	if (paciente.altura != 0) {
-	    var imc = paciente.peso / (paciente.altura * paciente.altura);
-	    return imc;
-
-	} else {
-	  console.log("informe altura");
-	}
-	
-
-}
-
-
 
 var trPacientes = document.getElementsByClassName("paciente"); //array que pega as trs 
 
@@ -26,10 +13,24 @@ for (var posicaoAtual = 0; posicaoAtual <= trPacientes.length -1 ; posicaoAtual+
 	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
 	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-	var pacienteAtual = {nome:tdNome.textContent, peso:tdPeso.textContent, altura:tdAltura.textContent}; //constroi o objeto
+	var pacienteAtual = {nome:tdNome.textContent, 
+						peso:tdPeso.textContent, 
+						altura:tdAltura.textContent,
+						pegaImc: function () { //função anonima
+							if (this.altura != 0) {
+	    						var imc = this.peso / (this.altura * this.altura);
+	    						return imc;
+
+							} else {
+	  							console.log("informe altura");
+							}
+							}
+
+
+						}; //constroi o objeto
 
 	
-	var imc = calculaImc(pacienteAtual);
+	var imc = pacienteAtual.pegaImc(); //recebe o imc da function e joga na var
 
 	var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
 	tdImc.textContent = imc; 
