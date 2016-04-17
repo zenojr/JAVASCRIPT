de6d96adbdb43a49b6c225f4de255533eabf49ad
zenojr <zfrancajunior@gmail.com>
2016-04-17 15:56:58 -0300

    troca de while por for
    Trocado o uso de While por fors

--- INTRODUCAO/projeto/calcula-imc.js
+++ INTRODUCAO/projeto/calcula-imc.js
@@ -1,36 +1,49 @@
 
 //textContent seta o valor coletado no DOM para a variavel
 
-var tdPeso1 = document.getElementById("peso-1");
-var tdAltura1 = document.getElementById("altura-1");
-var paciente1 = { peso : tdPeso1.textContent, altura : tdAltura1.textContent};
+//var tdPeso1 = document.getElementById("peso-1");
+//var tdAltura1 = document.getElementById("altura-1");
+//var paciente1 = { peso : tdPeso1.textContent, altura : tdAltura1.textContent};
 //primeiro paciente FIM
 
-var tdPeso2 = document.getElementById("peso-2");
-var tdAltura2 = document.getElementById("altura-2");
-var paciente2 = { peso : tdPeso2.textContent, altura : tdAltura2.textContent};
+//var tdPeso2 = document.getElementById("peso-2");
+//var tdAltura2 = document.getElementById("altura-2");
+//var paciente2 = { peso : tdPeso2.textContent, altura : tdAltura2.textContent};
 //segundo paciente FIM 
 
-var pacientes = [paciente1, paciente2];
-var posicaoAtual = 0;
-
-var primeiro = pacientes[0];
-var segundo = pacientes[1];
-
-while (posicaoAtual <= pacientes.length -1) {
-	// statement
+//var pacientes = [paciente1, paciente2];
 
 
-if (paciente.altura != 0) {
-    var imc = paciente.peso / (paciente.altura * paciente.altura);
 
-    var tdImc = document.getElementById("imc-2");
-    tdImc.textContent = imc; 
 
-    console.log(imc);
 
-} else {
-  console.log("informe altura");
-}
+var trPacientes = document.getElementsByClassName("paciente"); //array que pega as trs 
 
-}//endWhile
+
+for (var posicaoAtual = 0; posicaoAtual <= trPacientes.length -1 ; posicaoAtual++) {
+	
+
+	
+	var pacienteTr = trPacientes[posicaoAtual]; //array class paciente
+	
+	//pega elementos dentro do array pacienteTr com variavle de controle
+	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+
+	var paciente = {nome:tdNome.textContent, peso:tdPeso.textContent, altura:tdAltura.textContent};
+
+	if (paciente.altura != 0) {
+	    var imc = paciente.peso / (paciente.altura * paciente.altura);
+
+	    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+	    tdImc.textContent = imc; 
+
+	    console.log(imc);
+
+	} else {
+	  console.log("informe altura");
+	}
+	posicaoAtual++;
+
+};//endFor
