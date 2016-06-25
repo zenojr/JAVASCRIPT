243d3ab1e22b63fb7593e8a8d354f696f9e39bbd
zenojr <zfrancajunior@gmail.com>
2016-06-25 20:58:52 -0300

    Adicionados botões remove
    Adicionados botões remove e troca de cores também

--- JAVASCRIPT_WEB_APP/javascript/card.js
+++ JAVASCRIPT_WEB_APP/javascript/card.js
@@ -1,13 +1,20 @@
 var $cards = document.querySelectorAll('.card');
-var $cardColors = document.querySelectorAll('.card-colors');
+var $cardColors = document.querySelectorAll('.card-options');
 
 for(var index = 0; index < $cards.length; index++){
 	$cards[index].addEventListener('click', function(event){
 		var $origin = event.target; 
+		console.log($origin);
 		var $card = this; 
+		console.log($card);
 
-		if ($origin.classList.contains('card-colors')) {
+		if ($origin.classList.contains('card-options')) {
 			$card.dataset.color = $origin.dataset.color;
+			//seleciona pelo evento click a classe do elemento clicado
+
+		if ($origin.classList.contains('card-delete')){
+			$card.remove();
+		}
 
 		for(var position = 0; position < $cardColors.length; position++){
 			$cardColors[position].classList.remove('isActive');
@@ -16,6 +23,6 @@ for(var index = 0; index < $cards.length; index++){
 		};
 		
 
-		
+
 	});
 };
